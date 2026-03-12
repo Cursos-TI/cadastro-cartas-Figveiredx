91fc7a0eae108567cbaa3c1ebd0f8478ff68c3bd
Figveiredx <renan_figueiredo2@hotmail.com>
2026-03-12 16:47:02 +0000

    Apenas upei os codigos que tinha feito vscode.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,70 @@
 #include <stdio.h>
+int main(){
+    
+  int populacao;
+  int pontos;
+  float area;
+  float pib;
+  char cidade[50];
+  char carta[3];
+char estado;
+printf("    SUPER TRUNFO  beta  \n"); 
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+printf("Digite o estado: \n");
+scanf("%s", &estado);
 
-  // Área para entrada de dados
+  printf("Digite a cidade: \n");
+  scanf("%s", &cidade);
 
-  // Área para exibição dos dados da cidade
+ printf("Digite a populacao da cidade: \n");
+ scanf("%d", &populacao);
+
+  printf("Digite qual a area da cidade: \n");
+  scanf("%f", &area);
+
+  printf("Diga o Pib da cidade: \n");
+  scanf("%d", &pib);
+
+  printf("Quantos pontos turisticos há na cidade: \n");
+  scanf("%d", &pontos);
+  
+
+  //CARTA 2//
+  //variaveis carta 2
+  char estado2, cidade2;
+  int populacao2, pontos2;
+  float area2, pib2;
+printf("Carta 2\n");
+printf("Declaração da Segunda carta\n");
+
+
+printf("Digite o estado: \n");
+scanf("%s", &estado2);
+
+  printf("Digite a cidade: \n");
+  scanf("%s", &cidade2);
+
+ printf("Digite a populacao da cidade: \n");
+ scanf("%d", &populacao2);
+
+  printf("Digite qual a area da cidade: \n");
+  scanf("%f", &area2);
+
+  printf("Diga o Pib da cidade: \n");
+  scanf("%d", &pib2);
+
+  printf("Quantos pontos turisticos há na cidade: \n");
+  scanf("%d", &pontos2);
+
+
+  
+
+
+
+
+
+
 
 return 0;
-} 
+}
